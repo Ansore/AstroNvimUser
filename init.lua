18dd90035f274955d36e8cb53acebe80255ab2cf
Ansore <aansore@outlook.com>
2024-04-17 00:44:51 +0800

    turn off the lsp log

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
+-- vim.lsp.set_log_level('debug')
+vim.lsp.set_log_level('off')
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
